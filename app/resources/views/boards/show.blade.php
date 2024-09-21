dedbe261644f39caa6e01f2cca1c950cb717d486
Danny Yan <yandanny2004@gmail.com>
2024-09-21 15:42:35 +1000

    Updated show board to be formatted correctly

--- app/resources/views/boards/show.blade.php
+++ app/resources/views/boards/show.blade.php
@@ -2,13 +2,13 @@
 
     <!-- Meta tag for CSRF token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <x-top-bar :title="$board->name"/>
 
     <div class="container mx-auto mt-8">
         <div class="flex flex-col">
-            <x-top-bar :title="$board->name"/>
 
             <div class="flex pr-10 items-start dark:text-white">
-                <p class = "text-sm px-6 min-h-20 max-h-40 overflow-y-auto max-w-[65vw]">This is a description of the board.</p>
+                <p class = "text-sm px-6 min-h-20 max-h-40 overflow-y-auto max-w-[65vw] grow">This is a description of the board.</p>
                 <div class="flex space-x-8 items-center">
                     <p class = "lg:block hidden">X-days-left</p>
                     <x-secondary-button>Complete Board</x-secondary-button>
