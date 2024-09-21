4bfac520719293f43ba7f48567fee6d7eda18935
Panshul Kedaar Kale <pkal0016@student.monash.edu>
2024-09-21 12:32:01 +1000

    Fix hovers for dark mode

--- app/resources/views/components/task-box.blade.php
+++ app/resources/views/components/task-box.blade.php
@@ -16,7 +16,7 @@
     </div>
     <div class = "flex flex-col justify-between items-center">
         <details class="open">
-            <summary class="hover:cursor-pointer hover:bg-gray-100 bg-opacity-10 list-none">
+            <summary class="hover:cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-700 bg-opacity-10 list-none">
                 <i class="fa-solid fa-ellipsis px-2"></i>
             </summary>
             <x-task-detail task=""/>
