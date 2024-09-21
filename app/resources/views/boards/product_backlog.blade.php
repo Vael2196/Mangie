738e8992e751740c89789ef3895634d858199d25
Danny Yan <yandanny2004@gmail.com>
2024-09-21 11:09:54 +1000

    Created table template

--- app/resources/views/boards/product_backlog.blade.php
+++ app/resources/views/boards/product_backlog.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-top-bar title="Product Backlog"/>
-    {{-- <div class="hidden sm:flex sm:items-center sm:ms-6">
+    <div class="hidden sm:flex sm:items-center sm:ms-6">
         <x-dropdown align="right" width="48">
             <x-slot name="trigger">
                 <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
@@ -24,9 +24,9 @@
                 </form>
             </x-slot>
         </x-dropdown>
-    </div> --}}
+    </div>
     <div class="px-10 flex flex-col w-[80vw] overflow-x-auto">
-        <h1 class='mb-2'>Issue</h1>
+        <h1 class='mb-2'>Issues: Number</h1>
         {{-- <x-task-board>
             <x-task-box DESP="To make a task"/>
             <x-task-box DESP="To make a task"/>
