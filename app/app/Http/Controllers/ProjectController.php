8614e9ecbd7ea4535d342906543876da4c960c37
Panshul Kedaar Kale <pkal0016@student.monash.edu>
2024-10-02 00:44:38 +1000

    Status column

--- app/app/Http/Controllers/ProjectController.php
+++ app/app/Http/Controllers/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class ProjectController extends Controller
 {
