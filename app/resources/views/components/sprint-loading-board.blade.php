0c0e49a94fb1c18e9d7ae167d1317fbb974aeca1
Danny Yan <yandanny2004@gmail.com>
2024-10-07 12:12:03 +1100

    Added start sprint button

--- app/resources/views/components/sprint-loading-board.blade.php
+++ app/resources/views/components/sprint-loading-board.blade.php
@@ -1,7 +1,10 @@
 @props(['name'])
 
 <div {{ $attributes->merge([ "class" => "border min-w-full overflow-x-auto rounded p-3 bg-gray-100"])}}>
-    <h1 class="mb-3 font-semibold">{{$name}}</h1>
+    <div class="flex justify-between mb-2">
+        <h1 class="font-semibold text-xl">{{$name}}</h1>
+        <x-secondary-button>Start Sprint</x-secondary-button>
+    </div>
     @if ($slot->isNotEmpty())
     <table {{ $attributes->merge([ "class" => "table-fixed border min-w-full overflow-x-auto rounded bg-white"])}}>
         {{ $slot }}
