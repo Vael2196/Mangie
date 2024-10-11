0b0954abcf046d23b0ae81e726445dbc740b6f99
Panshul Kedaar Kale <pkal0016@student.monash.edu>
2024-10-11 20:12:48 +1100

    create a profile detail when clicking the profile icon

---
+++ app/resources/views/components/profile-detail.blade.php
@@ -0,0 +1,5 @@
+<div class = "absolute top-0 right-0 h-full w-full p-5 flex justify-center items-center z-30 backdrop-blur-sm bg-black bg-opacity-30">
+    <div class = "rounded lg:px-11 lg:py-7 p-5 lg:w-4/6 w-5/6 h-3/5 bg-white dark:bg-gradient-to-l from-slate-700 to-gray-900 border-2 flex">
+        
+    </div>
+</div>
