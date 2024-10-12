eaa5bc7a685d4d13026e9d0a8cffee12ae571669
Danny Yan <yandanny2004@gmail.com>
2024-10-13 09:03:57 +1100

    Retained selected item when reloading the page

--- app/resources/views/components/sprint-loading-board-card.blade.php
+++ app/resources/views/components/sprint-loading-board-card.blade.php
@@ -3,7 +3,7 @@
         <h1 class="font-semibold text-xl">{{$board->name}}</h1>
         <a href="{{ route('boards.show', $board->id) }}"><x-secondary-button>Start Sprint</x-secondary-button></a>
     </div>
-    <div class="rounded w-full h-full bg-slate-100 p-3 m-3 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-flow-dense gap-2 justify-items-center items-start">
+    <div class="rounded h-full bg-slate-100 m-3 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 grid-flow-dense gap-2 justify-items-center items-start">
         {{$slot}}
     </div>
 </div>
