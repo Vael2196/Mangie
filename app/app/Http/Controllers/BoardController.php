ca79d841cf7f79fd7a0eb7224ae9e9686b096e06
Danny Yan <yandanny2004@gmail.com>
2024-10-13 13:42:53 +1100

    Fixed UI issues relating to card view box

--- app/app/Http/Controllers/BoardController.php
+++ app/app/Http/Controllers/BoardController.php
@@ -144,7 +144,7 @@ class BoardController extends Controller
 
         // Get All boards for a user, including the product backlog
         $user = Auth::user();
-        $boards = Board::where('user_id', $user->id)->whereNot('id', 1)->get();
+        $boards = Board::where('user_id', $user->id)->orWhere('id', 1)->get();
         $tasks = Task::all();
 
         if ($view == 'card'){
