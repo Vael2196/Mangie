b41bbf02e97b7c848944ae35b1d4ef169226d5ab
Danny Yan <yandanny2004@gmail.com>
2024-10-13 16:42:42 +1100

    Added time log and priority columns to tasks

--- app/app/Http/Controllers/BoardController.php
+++ app/app/Http/Controllers/BoardController.php
@@ -215,7 +215,9 @@ class BoardController extends Controller
             'description' => 'nullable|string',
             'assignee' => 'integer',
             'labels' => 'nullable|string',
+            'priority' => 'nullable|string',
             'storyPoint' => 'integer',
+            'timeLog' => 'integer',
         ]);
 
         // Update the task
@@ -224,7 +226,9 @@ class BoardController extends Controller
             'column_id' => $request->column_id,
             'description' => $request->description,
             'labels' => $request->labels,
+            'priority' => $request->priority,
             'story_points' => $request->storyPoint,
+            'time_log' => $request->timeLog,
             'updated_at' => now()
         ]);
 
