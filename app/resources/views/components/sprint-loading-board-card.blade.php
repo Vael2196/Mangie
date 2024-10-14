c1bf54022c5bbcf980780d5c3a5ad1bff1e76060
Panshul Kedaar Kale <pkal0016@student.monash.edu>
2024-10-14 12:58:44 +1100

    dark mode for create sprint button

--- app/resources/views/components/sprint-loading-board-card.blade.php
+++ app/resources/views/components/sprint-loading-board-card.blade.php
@@ -1,7 +1,7 @@
 <div {{ $attributes->merge([ "class" => "border min-w-full overflow-x-auto rounded p-3 bg-gray-100 dark:bg-gray-700"])}}>
     <div class="flex justify-between mb-2">
         <h1 class="font-semibold text-xl">{{$board->name}}</h1>
-        <a href="{{ route('boards.show', $board->id) }}"><x-secondary-button>Start Sprint</x-secondary-button></a>
+        <x-sprint-start-details :board="$board"/>
     </div>
     @if($slot->isNotEmpty())
         <div class="rounded h-full bg-slate-100 m-3 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 grid-flow-dense justify-items-center items-start dark:bg-gray-700">
