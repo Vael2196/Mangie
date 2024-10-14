69aef731cdeb6080b7fab27428af4dc788afa755
Panshul Kedaar Kale <pkal0016@student.monash.edu>
2024-10-14 15:17:05 +1100

    Add feature so that only uncompleted sprints show in home page

--- app/resources/views/boards/show.blade.php
+++ app/resources/views/boards/show.blade.php
@@ -17,13 +17,7 @@
 
                 <div class="flex space-x-8 items-center">
 
-                    @if ($activeSprints >= 1)
-                        @if ($board->status == 1)
-                            <x-sprint-start-details :board="$board"/>
-                        @endif
-                    @elseif ($board->completed == 0)
-                        <x-sprint-start-details :board="$board"/>
-                    @endif
+                    <x-sprint-start-details :board="$board" :daysLeft="$daysLeft"/>
 
 
                     {{-- <select id="activateSprint" class="bg-white dark:bg-gray-700 dark:text-white rounded-lg p-2">
@@ -53,7 +47,7 @@
                         <p class="lg:block hidden">Sprint is not active</p>
                     @endif
 
-                    @if ($daysLeft != null && $daysLeft >= 0 && $board->completed == 0)
+                    @if ($board->completed == 0 && $board->status == 1)
                         <form method="POST" action="{{ route('boards.complete', $board->id) }}" onsubmit="return confirm('Are you sure you want to complete the sprint early?')">
                             @csrf
                             <button type="submit" class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150">
